c54d07c9a44d376f0f805aae6e38f82426d362d5
Victor Mutai <victormutaijambo@gmail.com>
2025-06-18 01:22:55 +0300

    revert tanstack. use globalThis

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { CreateIdentity } from "./CreateIdentity";
-import {
-  Outlet,
-  RouterProvider,
-  createRouter,
-  createRoute,
-  createRootRoute,
-} from "@tanstack/react-router";
 import CreateJWK from "./CreateJWK";
 
-const root = document.querySelector("#root");
-
-const rootRoute = createRootRoute({
-  component: () => <Outlet />,
-});
-
-const indexRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/",
-  component: function Index() {
-    return (
-      <div className="mx-auto container mt-10 w-full">
+const App = () => {
+  return (
+    <div className="mx-auto container mt-10 w-full">
+      {globalThis.location.hash === "#create-jwk" ? (
+        <CreateJWK />
+      ) : (
         <CreateIdentity />
-      </div>
-    );
-  },
-});
+      )}
+    </div>
+  );
+};
 
-const createJWKRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: '/create-jwk',
-  component: CreateJWK,
-})
-
-const routeTree = rootRoute.addChildren([indexRoute, createJWKRoute]);
-
-const router = createRouter({ routeTree });
+const root = document.querySelector("#root");
 
 if (root) {
   ReactDOM.createRoot(root as HTMLElement).render(
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <App />
     </React.StrictMode>
   );
 }
